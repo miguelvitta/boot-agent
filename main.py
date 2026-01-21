8ed83744a742806cc3a653f5643469d43b9ed682
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-21 11:33:16 -0300

    progress on the agent and a little change to pyproject

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise RuntimeError("GEMINI_API_KEY not set")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    apiKey = os.environ.get("GEMINI_API_KEY")
+    if not apiKey:
+        raise RuntimeError("GEMINI_API_KEY not set")
+
+    client = genai.Client(api_key=apiKey)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    metadata = response.usage_metadata
+
+    print("Prompt tokens:", metadata.prompt_token_count)
+    print("Response tokens:", metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
