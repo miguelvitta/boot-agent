59806d38ecc81f25cd7626fa2ebc3ddcf525e125
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-21 12:29:08 -0300

    feat: adds a verbose flag that shows extra information

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -18,10 +19,10 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client, messages)
+    generate_content(client, messages, args.verbose, args.user_prompt)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose, user_prompt):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -29,8 +30,10 @@ def generate_content(client, messages):
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("User prompt:", user_prompt)
     print("Response:")
     print(response.text)
 
