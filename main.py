c7bbfeae9b6e7cade4ead5b793c5a22eb06d5765
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-21 10:59:55 -0300

    some build adjustments

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY not set")
+
+client = genai.Client(api_key=api_key)
