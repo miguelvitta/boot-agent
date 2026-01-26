e3d8ae398cd6849b1554b1138872855b403c39ca
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-26 13:33:02 -0300

    style: changes the spaces a little

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,8 +5,10 @@ def get_file_content(working_directory, file_path):
     try:
         abs_working_dir = os.path.abspath(working_directory)
         target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
         if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
         if not os.path.isfile(target_file):
                 return f'Error: File not found or is not a regular file: "{file_path}"'
         
@@ -15,6 +17,8 @@ def get_file_content(working_directory, file_path):
             # testing if the file is larger thant the limit
             if f.read(1):
                 file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
         return file_content_string
+    
     except Exception as e:
         return f"Error: when reading the file {file_path}: {e}"    
--- functions/write_file.py
+++ functions/write_file.py
@@ -4,8 +4,10 @@ def write_file(working_directory, file_path, content):
     try:
         abs_working_dir = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
         if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        
         if os.path.isdir(target_dir):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
         
@@ -13,6 +15,8 @@ def write_file(working_directory, file_path, content):
 
         with open(target_dir, "w") as f:
             f.write(content)
+
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
     except Exception as e:
         return f"Error: writing to file: {e}"
